b973bb22dca8780eb63b5f6d93cb2fce1dd3cadc
nixbitcoin <nixbitcoin@i2pmail.org>
2021-04-20 10:32:08 +0200

    update nixpkgs

--- pkgs/nixpkgs-pinned.nix
+++ pkgs/nixpkgs-pinned.nix
@@ -8,11 +8,11 @@ in
 {
   # To update, run ../helper/fetch-channel REV
   nixpkgs = fetch {
-    rev = "d6f63659a7021051a46035373ed50fbea7e4e924";
-    sha256 = "0vblhzg57sfzqpdm24lgs08vjv2204lzcp6hv4cbjd20rz0mxs4y";
+    rev = "9a1672105db0eebe8ef59f310397435f2d0298d0";
+    sha256 = "06z4r0aaha5qyd0prg7h1f5sjrsndca75150zf5w4ff6g9vdv1rb";
   };
   nixpkgs-unstable = fetch {
-    rev = "9e377a6ce42dccd9b624ae4ce8f978dc892ba0e2";
-    sha256 = "1r3ll77hyqn28d9i4cf3vqd9v48fmaa1j8ps8c4fm4f8gqf4kpl1";
+    rev = "0a5f5bab0e08e968ef25cff393312aa51a3512cf";
+    sha256 = "161xisa5a7wi2h5hs0p1w7zbf095w21cs0wp0524kp7p6cpvvfyn";
   };
 }
