1e4534775ecc5856976eeabb916efe62c1b28cb0
nixbitcoin <nixbitcoin@i2pmail.org>
2021-12-22 14:11:49 +0100

    update nixpgks
    Includes i2pd 2.40.0

--- pkgs/nixpkgs-pinned.nix
+++ pkgs/nixpkgs-pinned.nix
@@ -8,11 +8,11 @@ in
 {
   # To update, run ../helper/fetch-channel REV
   nixpkgs = fetch {
-    rev = "e3699f9a4be7f9e12e697e7dc8ee6a0ec790e9e1";
-    sha256 = "0dqgwmpfmzcinfk21kqmvjgwm6j0dlc9pd2614my5dfjjl9nfx9w";
+    rev = "9ab7d12287ced0e1b4c03b61c781901f178d9d77";
+    sha256 = "0bbd2pgcyavqn5wgq0xp8p67lha0kv9iqnh49i9w5fb5g29q7i30";
   };
   nixpkgs-unstable = fetch {
-    rev = "2deb07f3ac4eeb5de1c12c4ba2911a2eb1f6ed61";
-    sha256 = "0036sv1sc4ddf8mv8f8j9ifqzl3fhvsbri4z1kppn0f1zk6jv9yi";
+    rev = "81cef6b70fb5d5cdba5a0fef3f714c2dadaf0d6d";
+    sha256 = "1mj9psy1hfy3fbalwkdlyw3jmc97sl9g3xj1xh8dmhl68g0pfjin";
   };
 }
